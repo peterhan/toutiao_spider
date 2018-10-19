7b31ae516afc76361f5ee40b52844a00dd5fb5be
hanshu <hanshu@autohome.com.cn>
2018-10-19 16:43:55 +0800

    fix

--- toutiaohao_crawler.py
+++ toutiaohao_crawler.py
@@ -1,6 +1,7 @@
 import requests
 import json
-import math,time,hashlib
+import math,hashlib
+import datetime,time
 import PyV8
 
 UA_ST = '''Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0'''
@@ -8,8 +9,33 @@ UA_ST = '''Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like G
 UA_js = '''var navigator = {};
 navigator["userAgent"] = "%s";
 '''%UA_ST
-
 headers = {"User-Agent":UA_ST}    
+
+DATE_FORMAT='%Y-%m-%d %H:%M:%S'
+
+def get_date(fmt=DATE_FORMAT,base= datetime.datetime.now(), isobj=False, **kwargs ):
+    i_str2date=lambda str_date,fmt: datetime.datetime.fromtimestamp(time.mktime(time.strptime(str_date,fmt)))
+    if type(base)==str:
+        dateobj= i_str2date(base,fmt)+ datetime.timedelta( **kwargs)
+    else:
+        dateobj = base + datetime.timedelta( **kwargs)
+    if isobj: 
+        return dateobj
+    else: 
+        return dateobj.strftime(fmt)
+        
+        
+def unix2ts(uxts,mask=DATE_FORMAT,base=10):
+    return \
+        datetime.datetime.fromtimestamp( 
+         int(uxts,base) 
+        ).strftime(mask)
+        
+def ts2unix(str_date,mask=DATE_FORMAT):
+    return \
+        int(time.mktime(
+         time.strptime(str_date, mask)
+        )) 
 
 def getASCP():
     t = int(math.floor(time.time()))
@@ -56,31 +82,59 @@ def get_veri_data(uid,maxhot='0'):
     sig =get_signature(uid,maxhot)
     return {'uid':uid,'_as':_as,'cp':cp,'sig':sig}
 
-def dump_json(jo):
-    return json.dumps(jo,ensure_ascii=False,indent=2).encode('gbk','ignore')  
+def dump_json(jo,i=None,e='gbk'):
+    return json.dumps(jo,ensure_ascii=False,indent=i).encode(e,'ignore')  
     
-def get_index(cat='news_car'):
-    maxhot = '1539912409'
+def get_index(cat='news_car',maxhot = '1539912409'):    
     data =  get_veri_data('', maxhot=maxhot) 
     data['cat'] = cat
     data['maxhot'] = maxhot
-    url='https://www.toutiao.com/api/pc/feed/?category={cat}&utm_source=toutiao&widen=50&max_behot_time={maxhot}&max_behot_time_tmp={maxhot}&tadrequire=true&as={_as}&cp={cp}&_signature={sig}'.format(**data)
+    cookies={'uuid':'w:c1554d99c34047cc8e89fd','tt_webid':'6612754979269297671'}   
+    url='https://www.toutiao.com/api/pc/feed/?category={cat}&utm_source=toutiao&widen=1&max_behot_time={maxhot}&max_behot_time_tmp={maxhot}&tadrequire=true&as={_as}&cp={cp}&_signature={sig}'.format(**data)
     print url
-    rp = requests.get(url,headers=headers)
-    print dump_json(rp.json())
+    rp = requests.get(url,headers=headers,cookies=cookies)
+    jo = rp.json()
+    open('dbg.js','w').write(dump_json(jo))
+    sl = []
+    for d in jo['data']:
+        # print d.keys()
+        # print d
+        sl.append(dump_json({'src':d.get('source'),'url':d.get('media_url')} ,i=None,e='utf8'))
+        print d.get('title','').encode('gbk','ignore')
+        # print dump_json()
+    open('source_list.txt','w').write('\n'.join(sl))
 
     
 def get_uid_page(uid):    
     # print uj.keys()
     data = get_veri_data(uid)        
-    url='https://www.toutiao.com/c/user/article/?page_type=1&user_id={uid}&max_behot_time=0&count=20&as={_as}&cp={cp}&_signature={sig}'.format(**data)
+    url='https://www.toutiao.com/c/user/article/?page_type=1&user_id={uid}&max_behot_time=0&count=200&as={_as}&cp={cp}&_signature={sig}'.format(**data)
     res = requests.get(url,headers=headers)
     print url
-    jst = dump_json(res.json())     
-    print >>fo,'\t'.join([str(uid),url,jst])
+    jo = res.json()
+    jo.update({'uid':uid,'sig_data':data})
+    jst = dump_json(jo) 
+    print jo['data'][0].keys()
+    return jo
     # break
 
-
+def user_crawl():
+    res = []
+    for row in open('source_list.txt'):
+        obj=json.loads(row)
+        jo = get_uid_page(obj['url'].split('/')[-2])
+        res.append(dump_json(jo,i=None,e='utf8'))
+    open('upage.txt','a').write('\n'.join(res))
+        
+    
+def index_crawl():
+    cols='news_finance,news_entertainment,news_travel,news_car,car_new_arrival,SUV,car_guide,car_usage,news_hot'.split(',')
+    for col in cols[1:2]:
+        for i in range(10):
+            mh = str(ts2unix(get_date())-i*2000)
+            print mh
+            get_index(col, mh)
+        
 if __name__=='__main__':
-    for col in 'news_car,car_new_arrival,SUV,car_guide,car_usage,news_hot'.split(','):
-        get_index(col)
+    # index_crawl()
+    user_crawl()
